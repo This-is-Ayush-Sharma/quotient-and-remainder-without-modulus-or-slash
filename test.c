4aaa424e34a59177d535c4ce85e2d92d73cfaa7b
Ayush Sharma <ThisIsAyush79@gmail.com>
2021-05-24 18:22:17 +0530

    code upload done....!

---
+++ test.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int div,d,flag=0;
+    scanf("%d%d",&div,&d);
+	int i,j;
+	for(i=div;i>0;i--)
+	{
+		for(j=1;j<=i;j++)
+		{
+			if((j*d)==i)
+			{
+			    flag=1;
+				printf("quotient is %d \nremainder is %d",j,div-(j*d));
+				break;
+			}
+		}
+		if(flag==1)
+		break;
+	}
+	return 0;
+}
